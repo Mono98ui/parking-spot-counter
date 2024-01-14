f5c2917a919d412d3dfbe529d9c6aadcad76b4c9
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2024-01-14 14:38:06 -0500

    add graphical availeble spot

--- counter/src/main/java/org/parking_spot/Main.java
+++ counter/src/main/java/org/parking_spot/Main.java
@@ -6,22 +6,40 @@ import static java.lang.Thread.sleep;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
+    static int spot = 100;
+
+    static boolean[] parkingAvailable = new boolean[spot];
+
     public static int countNbrSpot(parkingSpot[] parking){
         int nbrSpot = 0;
         for (int i =0; i < parking.length; i++ ){
             synchronized  (parking[i]){
                 if(parking[i].isAvailable){
                     nbrSpot++;
+                    parkingAvailable[i] = true;
+                    continue;
                 }
+                parkingAvailable[i] = false;
             }
         }
+
+        for (int i =0; i < parkingAvailable.length; i++ ){
+            String av = parkingAvailable[i] ? "*":" ";
+            if(i % 10 == 0){
+                System.out.println();
+                System.out.println("=======================");
+            }
+            System.out.print("|"+av);
+        }
+        System.out.println();
+        System.out.println("=======================");
 
 
         return nbrSpot;
     }
     public static void main(String[] args) {
 
-        parkingSpot[] parking = new parkingSpot[100];
+        parkingSpot[] parking = new parkingSpot[spot];
 
         for (int i =0; i < parking.length; i++ ){
             parking[i] = new parkingSpot();
@@ -33,7 +51,7 @@ public class Main {
         while (true){
             try {
                 sleep(10000);
-                System.out.println(countNbrSpot(parking));
+                System.out.println(String.valueOf(countNbrSpot(parking))+" available spots");
 
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
