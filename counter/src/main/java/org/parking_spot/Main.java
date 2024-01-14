ae75a61d4b1dc37fbc9020b27a57fcadf3762b07
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2024-01-13 20:07:48 -0500

    create thread

--- counter/src/main/java/org/parking_spot/Main.java
+++ counter/src/main/java/org/parking_spot/Main.java
@@ -4,16 +4,13 @@ package org.parking_spot;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        parkingSpot p1 = new parkingSpot();
+        p1.start();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        try{
+            p1.join();
+        }catch(InterruptedException e){
+            e.printStackTrace();
         }
     }
 }
---
+++ counter/src/main/java/org/parking_spot/parkingSpot.java
@@ -0,0 +1,9 @@
+package org.parking_spot;
+
+public class parkingSpot extends Thread{
+    @Override
+    public void run() {
+        super.run();
+        System.out.println("Hello world");
+    }
+}
