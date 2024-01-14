5b204ebe408565793194a38ee2a52ecb9eebe213
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2024-01-14 14:24:28 -0500

    implement parking spot

--- counter/src/main/java/org/parking_spot/Main.java
+++ counter/src/main/java/org/parking_spot/Main.java
@@ -1,16 +1,44 @@
 package org.parking_spot;
 
+import static java.lang.Thread.sleep;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        parkingSpot p1 = new parkingSpot();
-        p1.start();
 
-        try{
-            p1.join();
-        }catch(InterruptedException e){
-            e.printStackTrace();
+    public static int countNbrSpot(parkingSpot[] parking){
+        int nbrSpot = 0;
+        for (int i =0; i < parking.length; i++ ){
+            synchronized  (parking[i]){
+                if(parking[i].isAvailable){
+                    nbrSpot++;
+                }
+            }
         }
+
+
+        return nbrSpot;
+    }
+    public static void main(String[] args) {
+
+        parkingSpot[] parking = new parkingSpot[100];
+
+        for (int i =0; i < parking.length; i++ ){
+            parking[i] = new parkingSpot();
+            parking[i].start();
+        }
+
+        System.out.println("All thread started");
+
+        while (true){
+            try {
+                sleep(10000);
+                System.out.println(countNbrSpot(parking));
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 }
--- counter/src/main/java/org/parking_spot/parkingSpot.java
+++ counter/src/main/java/org/parking_spot/parkingSpot.java
@@ -1,9 +1,36 @@
 package org.parking_spot;
 
+import java.util.Random;
+
 public class parkingSpot extends Thread{
+
+    boolean isAvailable = true;
+
+    public parkingSpot(){
+    }
+
+    public void available(){
+        this.isAvailable = true;
+    }
+
+    public void unavailable(){
+        this.isAvailable = false;
+    }
     @Override
     public void run() {
         super.run();
-        System.out.println("Hello world");
+        while(true){
+            Random rand = new Random();
+            try {
+                int sec = rand.nextInt(10000)+1000;
+                sleep(sec);
+                available();
+                sec = rand.nextInt(10000)+1000;
+                sleep(sec);
+                unavailable();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
